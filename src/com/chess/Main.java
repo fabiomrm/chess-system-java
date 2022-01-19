3db2799cb1ee1e95dbc5e74034fe2dc75567bb37
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-01-19 16:21:54 -0300

    chess layer and printing the board

--- src/com/chess/Main.java
+++ src/com/chess/Main.java
@@ -1,11 +1,12 @@
 package com.chess;
 
-import com.chess.boardgame.Board;
+import com.chess.chess.ChessMatch;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Board board = new Board(8, 8);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.gePieces());
     }
 }
